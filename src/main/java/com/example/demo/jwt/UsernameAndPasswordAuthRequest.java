8899d9004c480b619af2317dc72f58f14ca7d448
marko <marko@marko.com>
2022-01-27 17:35:42 +0100

    jwt security

---
+++ src/main/java/com/example/demo/jwt/UsernameAndPasswordAuthRequest.java
@@ -0,0 +1,18 @@
+package com.example.demo.jwt;
+
+public class UsernameAndPasswordAuthRequest {
+
+    private String username;
+    private String password;
+
+    public UsernameAndPasswordAuthRequest() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
